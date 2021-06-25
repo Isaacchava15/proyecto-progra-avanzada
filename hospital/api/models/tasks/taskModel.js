fd4ab15fa12a045cc524a39faa134c404dd308f8
Anthony Quiros Alfaro <39424423+tonyantqa@users.noreply.github.com>
2021-06-25 14:41:21 -0600

    Issac (#3)
    * borrar prueba
    
    * DatosPaciente

--- hospital/api/models/tasks/taskModel.js
+++ hospital/api/models/tasks/taskModel.js
@@ -5,7 +5,31 @@ var Schema = mongoose.Schema;
 var TaskSchema = mongoose.Schema({
     name: {
         type: String,
-        required: 'Kindly enter the name of the task'
+        required: 'Nombre del paciente'
+    },
+    apellidos: {
+        type: String,
+        required: 'Apellidos del paciente'
+    },
+    correo: {
+        type: String,
+        required: 'Correo del paciente'
+    },
+    numero: {
+        type: String,
+        required: 'Número de telefono del paciente'
+    },
+    cédula: {
+        type: String,
+        required: 'Número de cédula del paciente'
+    },
+    direccion: {
+        type: String,
+        required: 'Dirección del paciente'
+    },
+    consulta: {
+        type: String,
+        required: 'Consulta del paciente.'
     },
     create_date: {
         type: Date,
